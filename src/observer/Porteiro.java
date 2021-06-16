6d7b4a855be374669b32faffcad83418f6b9678b
Flavio Lima <flaviolima.s@live.com>
2021-06-16 17:08:27 -0300

    registrando evento no subject

--- src/observer/Porteiro.java
+++ src/observer/Porteiro.java
@@ -5,5 +5,8 @@ import java.util.List;
 
 public class Porteiro {
     private List<ObservadorChegadaAniversariante> observadores = new ArrayList<>();
-    
+
+    public void registrarObservador(ObservadorChegadaAniversariante evento) {
+        observadores.add(evento);
+    }
 }
