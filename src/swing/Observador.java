7357478688ef827d74c7cdaaf2e922c574f3f66e
Flavio Lima <flaviolima.s@live.com>
2021-06-16 19:33:00 -0300

    Centralizar o componente na tela do computador

--- src/swing/Observador.java
+++ src/swing/Observador.java
@@ -12,6 +12,7 @@ public class Observador {
         janela.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
         janela.setSize(600, 200);
         janela.setLayout(new SpringLayout());
+        janela.setLocationRelativeTo(null); // Centralizar o componente na tela do computador
 
         JButton button = new JButton("Clicar!");
         janela.add(button);
