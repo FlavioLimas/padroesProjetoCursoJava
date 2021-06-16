a0761780f88b15384ba8aebdcdfce6ae483cea4f
Flavio Lima <flaviolima.s@live.com>
2021-06-16 16:53:07 -0300

    definindo lista - obj que informara evento SUBJECT

--- src/observer/Porteiro.java
+++ src/observer/Porteiro.java
@@ -1,5 +1,9 @@
 package observer;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Porteiro {
+    private List<ObservadorChegadaAniversariante> observadores = new ArrayList<>();
     
 }
