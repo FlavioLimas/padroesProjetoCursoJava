7858c092af171c6bc17dbed5e38ce6a5b0083a77
Flavio Lima <flaviolima.s@live.com>
2021-06-16 19:17:23 -0300

    add button

--- src/swing/Observador.java
+++ src/swing/Observador.java
@@ -1,5 +1,6 @@
 package swing;
 
+import javax.swing.JButton;
 import javax.swing.JFrame;
 
 public class Observador {
@@ -9,6 +10,9 @@ public class Observador {
         JFrame janela = new JFrame("Observador");
         janela.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
         janela.setSize(600, 200);
+
+        JButton button = new JButton("Clicar!");
+        janela.add(button);
 
         janela.setVisible(true);
     }
