8863d620f32312a316d13b9127f4d99e0a84c67a
Flavio Lima <flaviolima.s@live.com>
2021-06-16 19:02:14 -0300

    implementando observer via funcional interface

--- src/observer/AniversarioSurpresa.java
+++ src/observer/AniversarioSurpresa.java
@@ -8,9 +8,10 @@ public class AniversarioSurpresa {
         Namorada namorada = new Namorada();
         porteiro.registrarObservador(namorada);
 
+        porteiro.registrarObservador(e -> {
+            System.out.println("Surpresa via lambda!");
+            System.out.println("Ocorreu em " + e.getMomento());
+        });
         porteiro.monitorar();
-
-        
-        porteiro.registrarObservador(e -> System.out.println("Surpresa via lambda!"));
     }
 }
